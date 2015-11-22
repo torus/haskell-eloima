8f603da2622e6a807aaafeb65c10c4a2090fa4dc
Hisai Toru <toru@torus.jp>
2015-11-22 13:38:41 +0900

    add some JSON message format

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,13 @@
 module Lib
     ( someFunc
     , aesonTest
+    , decodeCoord
+    , decodeActor
+    , decodeMove
+    , Coord(Coord)
+    , Actor(Actor)
+    , TimePos(TimePos)
+    , Move(Move)
     ) where
 
 import Control.Applicative ((<$>), (<*>), empty)
@@ -10,13 +17,35 @@ import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BL
 
 data Coord = Coord { x :: Double, y :: Double }
-             deriving (Show)
+             deriving (Show, Eq)
 
 -- A ToJSON instance allows us to encode a value as JSON.
 
 instance ToJSON Coord where
   toJSON (Coord xV yV) = object [ "x" .= xV,
                                   "y" .= yV ]
+
+data TimePos = TimePos { time :: Double, position :: Coord }
+               deriving (Show, Eq)
+
+instance ToJSON TimePos where
+  toJSON (TimePos time pos) = object [ "time" .= time,
+                                       "y" .= pos ]
+
+data Actor = Actor { id :: Int, pos :: Coord }
+             deriving (Show, Eq)
+
+instance ToJSON Actor where
+  toJSON (Actor id pos) = object [ "id" .= id,
+                                   "pos" .= pos ]
+
+data Move = Move { actorId :: Int, from :: TimePos, to :: TimePos }
+            deriving (Show, Eq)
+
+instance ToJSON Move where
+  toJSON (Move id from to) = object [ "id" .= id,
+                                      "from" .= from,
+                                      "to"   .= to]
 
 -- A FromJSON instance allows us to decode a value from JSON.  This
 -- should match the format used by the ToJSON instance.
@@ -27,8 +56,36 @@ instance FromJSON Coord where
                          v .: "y"
   parseJSON _          = empty
 
+instance FromJSON Actor where
+  parseJSON (Object a) = Actor <$>
+                         a .: "id" <*>
+                         a .: "pos"
+  parseJSON _          = empty
+
+instance FromJSON TimePos where
+  parseJSON (Object a) = TimePos <$>
+                         a .: "time" <*>
+                         a .: "pos"
+  parseJSON _          = empty
+
+instance FromJSON Move where
+  parseJSON (Object m) = Move <$>
+                         m .: "actor" <*>
+                         m .: "from" <*>
+                         m .: "to"
+  parseJSON _          = empty
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+decodeCoord :: BL.ByteString -> Maybe Coord
+decodeCoord = decode
+
+decodeActor :: BL.ByteString -> Maybe Actor
+decodeActor = decode
+
+decodeMove :: BL.ByteString -> Maybe Move
+decodeMove = decode
 
 aesonTest :: IO ()
 aesonTest = do
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,7 @@ import Test.QuickCheck
 import Control.Exception (evaluate)
 
 import Lib
+import qualified Data.ByteString.Lazy.Char8 as BL
 
 main :: IO ()
 main = hspec $ do
@@ -16,9 +17,23 @@ main = hspec $ do
     it "throws an exception if used with an empty list" $
       evaluate (head []) `shouldThrow` anyException
 
+  describe "Eq" $ do
+    it "compares doubles" $
+      (1234567890 :: Double) `shouldBe` (1234567890 :: Double)
+
   describe "Lib" $ do
     it "exists"
        someFunc
-
-    it "aesonTest" $ do
+    it "aesonTest"
        aesonTest
+
+  describe "Lib.decodeCoord" $ do
+    it "decodes Coord JSON" $
+       decodeCoord (BL.pack "{\"x\":3.0,\"y\":-1.0}") `shouldBe` Just (Coord 3.0 (-1.0))
+    it "decodes Actor JSON" $
+       decodeActor (BL.pack "{\"id\":12,\"pos\":{\"x\":3.0,\"y\":-1.0}}")
+                   `shouldBe` Just (Actor 12 (Coord 3.0 (-1.0)))
+    it "decodes Move JSON" $
+       decodeMove (BL.pack "{\"actor\":123,\"from\":{\"time\":1234567890,\"pos\":{\"x\":1,\"y\":2}},\"to\":{\"time\":1234767890,\"pos\":{\"x\":2,\"y\":2}}}")
+                  `shouldBe` Just (Move 123 (TimePos 1234567890 (Coord 1.0 2.0))
+                                            (TimePos 1234767890 (Coord 2.0 2.0)))
